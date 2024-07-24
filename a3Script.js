636813c3e21f554ba3bd6cee354c04795215d8d6
HaloArin7 <arinkhopkar@gmail.com>
2024-07-24 20:18:01 +0530

    Working A3 Commit

--- a3Script.js
+++ a3Script.js
@@ -1,28 +1,29 @@
-username = document.getElementById("username").value.trim();
-email = document.getElementById("email").value.trim();
-phoneNo = document.getElementById("phoneNo").value.trim();
-password = document.getElementById("password").value.trim();
-confirmPass = document.getElementById("confirmPass").value.trim();
-
 document.getElementById("studentTable").addEventListener("submit", function(event)
 {
     event.preventDefault();
-    errors = [];
+    let errors = [];
+
+    username = document.getElementById("username").value.trim();
+    email = document.getElementById("email").value.trim();
+    phoneNo = document.getElementById("phoneNo").value.trim();
+    password = document.getElementById("password").value.trim();
+    confirmPass = document.getElementById("confirmPass").value.trim();
+    
     if(username === "" || email === "" || phoneNo === "" || password === "" || confirmPass === "")
     {
         errors.push('One or more field is empty.');
     }
     
     phonePattern = /^\d{10}$/;
-    if(phonePattern.test(phoneNo) == false)
+    if(!phonePattern.test(phoneNo))
         errors.push("Phone number should be 10 digits.");
 
     emailPattern = /^[^\s@]+@[^\s@]{3,}\.[^\s@]{2,3}$/;
-    if(emailPattern.test(email) == false)
+    if(!emailPattern.test(email))
         errors.push("Invalid email.");
 
     passPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[&$#@]).{7,}$/;
-    if(passPattern.test(pass) == false)
+    if(!passPattern.test(password))
         errors.push("Password must be at least 7 characters long and contain at least one capital letter, one digit, and one special character from the set (&,$,#,@)")
 
     if(password !== confirmPass)
@@ -30,7 +31,8 @@ document.getElementById("studentTable").addEventListener("submit", function(even
 
     errorMessages = document.getElementById('errorMessages');
     errorMessages.innerHTML = '';
-    if (errors.length > 0) {
+    if (errors.length > 0) 
+    {
         errorMessages.innerHTML = errors.join('<br>');
     } 
     else 
