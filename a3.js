035ba22509e450f791166f35d8087afaafc0ad38
HaloArin7 <arinkhopkar@gmail.com>
2024-07-24 12:39:36 +0530

    A3Commit

---
+++ a3.js
@@ -0,0 +1,40 @@
+username = document.getElementById("username").value.trim();
+email = document.getElementById("email").value.trim();
+phoneNo = document.getElementById("phoneNo").value.trim();
+password = document.getElementById("password").value.trim();
+confirmPass = document.getElementById("confirmPass").value.trim();
+
+document.getElementById("studentTable").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+    errors = [];
+    if(username === "" || email === "" || phoneNo === "" || password === "" || confirmPass === "")
+    {
+        errors.push('One or more field is empty.');
+    }
+    
+    phonePattern = /^\d{10}$/;
+    if(phonePattern.test(phoneNo) == false)
+        errors.push("Phone number should be 10 digits.");
+
+    emailPattern = /^[^\s@]+@[^\s@]{3,}\.[^\s@]{2,3}$/;
+    if(emailPattern.test(email) == false)
+        errors.push("Invalid email.");
+
+    passPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[&$#@]).{7,}$/;
+    if(passPattern.test(pass) == false)
+        errors.push("Password must be at least 7 characters long and contain at least one capital letter, one digit, and one special character from the set (&,$,#,@)")
+
+    if(password !== confirmPass)
+        errors.push("Password and confirm password do not match.");
+
+    errorMessages = document.getElementById('errorMessages');
+    errorMessages.innerHTML = '';
+    if (errors.length > 0) {
+        errorMessages.innerHTML = errors.join('<br>');
+    } 
+    else 
+    {
+        errorMessages.innerHTML = 'Form submitted successfully!';
+    }
+});
