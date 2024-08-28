27dbce26a154d0768399cbbeef15091cfd38e9a7
HaloArin7 <arinkhopkar@gmail.com>
2024-08-28 11:20:11 +0530

    Completed React App

--- reactfsd/src/App.js
+++ reactfsd/src/App.js
@@ -4,41 +4,43 @@ import './App.css';
 
 
 function App() {
+	const [value, setValue] = useState('');
+
 	return (
     	<div className="container">
 			<div className='calculator'>
 				<form action=''>
-					<div>
-						<input type='text'></input>
+					<div className='display'>
+						<input type='text' value={value}></input>
 					</div>
 					<div>
-						<input type='button' value="Clear"></input>
-						<input type='button' value="⌫"></input>
-						<input type='button' value="." onClick={displayName}></input>
-						<input type='button' value="/" onClick={displayName}></input>
+						<input type='button' value="Clear" onClick={e => setValue('')}></input>
+						<input type='button' value="⌫" onClick={e => setValue(value.slice(0, -1))}></input>
+						<input type='button' value="." onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="/" onClick={e =>setValue(value + e.target.value)}></input>
  					</div>
 					<div>
-						<input type='button' value="7" onClick={displayName}></input>
-						<input type='button' value="8" onClick={displayName}></input>
-						<input type='button' value="9" onClick={displayName}></input>
-						<input type='button' value="*" onClick={displayName}></input>
+						<input type='button' value="7" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="8" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="9" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="*" onClick={e =>setValue(value + e.target.value)}></input>
  					</div>
 					<div>
-						<input type='button' value="4" onClick={displayName}></input>
-						<input type='button' value="5" onClick={displayName}></input>
-						<input type='button' value="6" onClick={displayName}></input>
-						<input type='button' value="+" onClick={displayName}></input>
+						<input type='button' value="4" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="5" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="6" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="+" onClick={e =>setValue(value + e.target.value)}></input>
  					</div>
 					<div>
-						<input type='button' value="1" onClick={displayName}></input>
-						<input type='button' value="2" onClick={displayName}></input>
-						<input type='button' value="3" onClick={displayName}></input>
-						<input type='button' value="-" onClick={displayName}></input>
+						<input type='button' value="1" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="2" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="3" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="-" onClick={e =>setValue(value + e.target.value)}></input>
  					</div>
 					<div>
-						<input type='button' value="00" onClick={displayName}></input>
-						<input type='button' value="0" onClick={displayName}></input>
-						<input type='button' value="="></input>
+						<input type='button' value="00" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="0" onClick={e =>setValue(value + e.target.value)}></input>
+						<input type='button' value="=" className='equal' onClick={e =>setValue(eval(value))}></input>
  					</div>
 				</form>
 			</div>
